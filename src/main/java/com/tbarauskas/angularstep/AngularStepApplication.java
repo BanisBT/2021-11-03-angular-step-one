6c1eaf9169a211767d125887ae52c139dace8916
BanisBT <banisbt@gmail.com>
2021-11-03 14:51:32 +0200

    first commit

---
+++ src/main/java/com/tbarauskas/angularstep/AngularStepApplication.java
@@ -0,0 +1,13 @@
+package com.tbarauskas.angularstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularStepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularStepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbarauskas/angularstep/AngularStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbarauskas.angularstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularStepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
